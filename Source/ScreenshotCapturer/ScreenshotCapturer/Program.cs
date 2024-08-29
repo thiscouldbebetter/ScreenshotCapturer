a6d84aab7a443721471a3b48d45eacd0754fe01c
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2024-08-28 23:56:43 -0400

    Split class ScreenshotCapturer off to its own file.  Renamed some command-line switches.  Added an --interactive switch, with a corresponding interactive mode.  Added the possibility of at-will, rather than timed, screenshots, as well as (possibly) infinite screenshots.

--- Source/ScreenshotCapturer/ScreenshotCapturer/Program.cs
+++ Source/ScreenshotCapturer/ScreenshotCapturer/Program.cs
@@ -1,42 +1,55 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
-using System.Threading;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace ScreenshotCapturer
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main(string[] argsAsArray)
 		{
 			// Usage:
-			// ScreenshotCapturer.exe [-prefix <filePathStemToSaveTo>] [-extension <imageFileExtension>]
-			// [-bounds <captureBoundsX> <captureBoundsY> <captureBoundsWidth> <captureBoundsHeight>]
-			// [-count <numberOfScreenshotsToCapture>] [-frequency <screenshotsPerSecond>]
+			// ScreenshotCapturer.exe [--imageFilePrefix=<filePathStemToSaveTo>] [--imageFileSuffix=<imageFileSuffix>]
+			// [-bounds=<left> <top> <width> <height>]
+			// [-screenshotsPerSecond=<screenshotsPerSecond>] [-screenshotsMax=<screenshotsMax>]
 			var now = DateTime.Now;
 			Console.WriteLine("Program begins: " + now);
+
 			const string dateTimeFormat = "yyyyMMdd-HHmmss-fff";
-			var filePathStemToSaveTo = "Screenshot-" + now.ToString(dateTimeFormat);
-			var imageFileExtension = ".png";
+
+			string imageFilePrefix = null;
+			string imageFileSuffix = null;
 			Rectangle? captureBoundsNullable = null;
-			var numberOfScreenshotsToCapture = 1;
-			var millisecondsPerScreenshot = 1000;
+			int? screenshotsPerSecond = null;
+			int? screenshotsToCaptureMax = null;
+
+			var imageFilePrefixDefault =
+				"Screenshot-" + now.ToString(dateTimeFormat);
+			const string imageFileSuffixDefault = ".png";
+			var captureBoundsDefault = Screen.PrimaryScreen.Bounds;
+			const int screenshotsPerSecondDefault = 1;
+			const int screenshotsToCaptureMaxDefault = 10;
+
 			try
 			{
-				for (var i = 0; i < args.Length; i++)
+				var args = argsAsArray.ToList();
+
+				for (var i = 0; i < args.Count; i++)
 				{
 					var argument = args[i];
-					if (argument == "-prefix")
+					if (argument == "--imageFilePrefix")
 					{
-						filePathStemToSaveTo = args[i + 1];
+						imageFilePrefix = args[i + 1];
 						i++;
 					}
-					else if (argument == "-extension")
+					else if (argument == "--imageFileSuffix")
 					{
-						imageFileExtension = args[i + 1];
+						imageFileSuffix = args[i + 1];
 						i++;
 					}
-					else if (argument == "-bounds")
+					else if (argument == "--bounds")
 					{
 						captureBoundsNullable = new Rectangle
 						(
@@ -47,19 +60,152 @@ namespace ScreenshotCapturer
 						);
 						i += 4;
 					}
-					else if (argument == "-count")
+					else if (argument == "--screenshotsPerSecond")
 					{
-						numberOfScreenshotsToCapture = int.Parse(args[i + 1]);
+						screenshotsPerSecond = int.Parse(args[i + 1]);
 						i++;
 					}
-					else if (argument == "-frequency")
+					else if (argument == "--screenshotsMax")
 					{
-						var screenshotsPerSecond = int.Parse(args[i + 1]);
-						millisecondsPerScreenshot = 1000 / screenshotsPerSecond; i++;
+						screenshotsToCaptureMax = int.Parse(args[i + 1]);
+						i++;
 					}
 					else
 					{
 						throw new Exception("Unrecognized command-line switch:" + argument);
+					}
+				}
+
+				var isInteractive =
+					args.Count == 0
+					|| args.Contains("--interactive");
+
+				if (isInteractive)
+				{
+					if (imageFilePrefix == null)
+					{
+						Console.Write($"Image File Prefix [{imageFilePrefixDefault}]:");
+						imageFilePrefix = Console.ReadLine();
+						if (imageFilePrefix == "")
+						{
+							imageFilePrefix = imageFilePrefixDefault;
+						}
+					}
+
+					if (imageFileSuffix == null)
+					{
+						Console.Write($"Image File Suffix [{imageFileSuffixDefault}]:");
+						imageFileSuffix = Console.ReadLine();
+						if (imageFileSuffix == "")
+						{
+							imageFileSuffix = imageFileSuffixDefault;
+						}
+					}
+					
+					if (captureBoundsNullable == null)
+					{
+						var captureBoundsDefaultAsString =
+							captureBoundsDefault.X
+							+ " " + captureBoundsDefault.Y
+							+ " " + captureBoundsDefault.Width
+							+ " " + captureBoundsDefault.Height;
+
+						int[] captureBoundsXYWidthAndHeight = null;
+
+						Console.Write($"Bounds to capture image within in pixels (left top width height) [{captureBoundsDefaultAsString }]:");
+						var captureBoundsAsString = Console.ReadLine();
+						if (captureBoundsAsString == "")
+						{
+							captureBoundsXYWidthAndHeight = new int[]
+							{
+								captureBoundsDefault.X, captureBoundsDefault.Y,
+								captureBoundsDefault.Width, captureBoundsDefault.Height
+							};
+						}
+						else
+						{
+							try
+							{
+								captureBoundsXYWidthAndHeight =
+									captureBoundsAsString
+										.Split(" ".ToCharArray())
+										.Select(x => int.Parse(x))
+										.ToArray();
+							}
+							catch (FormatException)
+							{
+								Console.WriteLine("Bounds did not have the expected format.  Using default.");
+								captureBoundsXYWidthAndHeight = new int[]
+								{
+									captureBoundsDefault.X, captureBoundsDefault.Y,
+									captureBoundsDefault.Width, captureBoundsDefault.Height
+								};
+							}
+						}
+
+						if (captureBoundsXYWidthAndHeight.Length != 4)
+						{
+							Console.WriteLine("Bounds did not have the expected format.  Using default.");
+							captureBoundsNullable = captureBoundsDefault;
+						}
+						else
+						{
+							captureBoundsNullable = new Rectangle
+							(
+								captureBoundsXYWidthAndHeight[0],
+								captureBoundsXYWidthAndHeight[1],
+								captureBoundsXYWidthAndHeight[2],
+								captureBoundsXYWidthAndHeight[3]
+							);
+						}
+					}
+
+					if (screenshotsPerSecond == null)
+					{
+						Console.Write($"Screenshots per second: (0 for user-triggered) [{screenshotsPerSecondDefault}]:");
+						var screenshotsPerSecondAsString = Console.ReadLine();
+						if (screenshotsPerSecondAsString == "")
+						{
+							screenshotsPerSecond = screenshotsPerSecondDefault;
+						}
+						else
+						{
+							try
+							{
+								screenshotsPerSecond = int.Parse(screenshotsPerSecondAsString);
+							}
+							catch (FormatException)
+							{
+								Console.WriteLine("Screenshots per second did not have expected format.  Using default.");
+								screenshotsPerSecond = screenshotsPerSecondDefault;
+							}
+						}
+					}
+
+					if (screenshotsToCaptureMax == null)
+					{
+						Console.Write($"Maximum number of screenshots to take (0 for infinity) [{screenshotsToCaptureMaxDefault}]:");
+						var numberOfScreenshotsToCaptureAsString = Console.ReadLine();
+						if (numberOfScreenshotsToCaptureAsString == "")
+						{
+							screenshotsToCaptureMax = screenshotsToCaptureMaxDefault;
+						}
+						else
+						{
+							try
+							{
+								screenshotsToCaptureMax = int.Parse(numberOfScreenshotsToCaptureAsString);
+								if (screenshotsToCaptureMax < 0)
+								{
+									Console.WriteLine("Number of screenshots to take did not have the expected format.  Using default.");
+								}
+							}
+							catch (FormatException)
+							{
+								Console.WriteLine("Number of screenshots to take did not have the expected format.  Using default.");
+								screenshotsToCaptureMax = screenshotsToCaptureMaxDefault;
+							}
+						}
 					}
 				}
 			}
@@ -69,84 +215,24 @@ namespace ScreenshotCapturer
 					"Error parsing command-line arguments: " + ex.Message;
 				Console.WriteLine(errorMessage);
 			}
+
 			var screenshotCapturer = new ScreenshotCapturer()
 			{
-				FilePathStemToSaveTo = filePathStemToSaveTo,
-				ImageFileExtension = imageFileExtension,
+				ImageFilePrefix = imageFilePrefix,
+				ImageFileSuffix = imageFileSuffix,
 				CaptureBoundsNullable = captureBoundsNullable,
-				NumberOfScreenshotsToCapture = numberOfScreenshotsToCapture,
-				MillisecondsPerScreenshot = millisecondsPerScreenshot
+				ScreenshotsPerSecond = screenshotsPerSecond.Value,
+				ScreenshotsToTakeMax = screenshotsToCaptureMax.Value,
 			};
+
 			screenshotCapturer.Start();
+
 			Console.WriteLine("Program ends: " + DateTime.Now);
-		}
-	}
-	public class ScreenshotCapturer
-	{
-		public string FilePathStemToSaveTo;
-		public string ImageFileExtension;
-		public Rectangle? CaptureBoundsNullable;
-		public int NumberOfScreenshotsToCapture;
-		public int MillisecondsPerScreenshot;
 
-		public void Start()
-		{
-			Console.WriteLine("-prefix=" + FilePathStemToSaveTo);
-			Console.WriteLine("-extension=" + ImageFileExtension);
-			Console.WriteLine("-bounds=" + CaptureBoundsNullable);
-			Console.WriteLine("-count=" + NumberOfScreenshotsToCapture);
-			Console.WriteLine("-frequency=" + (1000 / MillisecondsPerScreenshot));
-			if (NumberOfScreenshotsToCapture == 1)
+			if (Debugger.IsAttached)
 			{
-				CopyScreenToImageFile(0);
-			}
-			else
-			{
-				for (var i = 0; i < NumberOfScreenshotsToCapture; i++)
-				{
-					CopyScreenToImageFile(i);
-					Thread.Sleep(MillisecondsPerScreenshot);
-				}
-			}
-		}
-		public void CopyScreenToImageFile(int? imageIndex)
-		{
-			// Adapted from code found at the URL
-			// https://stackoverflow.com/questions/5049122/capture-the-screen-shot-using-net
-			Rectangle captureBounds;
-			if (CaptureBoundsNullable == null)
-			{
-				captureBounds = Screen.PrimaryScreen.Bounds;
-			}
-			else
-			{
-				captureBounds = CaptureBoundsNullable.Value;
-			}
-			var bitmapCaptured = new Bitmap(captureBounds.Width, captureBounds.Height);
-			var graphics = Graphics.FromImage(bitmapCaptured);
-			graphics.CopyFromScreen
-			(
-				captureBounds.X, captureBounds.Y, // source
-				0, 0, // destination
-				bitmapCaptured.Size,
-				CopyPixelOperation.SourceCopy
-			);
-			var filePathToSaveTo =
-				FilePathStemToSaveTo
-				+ (imageIndex == null ? "" : imageIndex.Value.ToString())
-				+ ImageFileExtension;
-
-			Console.WriteLine("Saving " + filePathToSaveTo + "...");
-
-			try
-			{
-				bitmapCaptured.Save(filePathToSaveTo);
-			}
-			catch (Exception)
-			{
-				var errorMessage =
-					"Error attemping to save file. Ensure directory exists, and permissions are adequate.";
-				Console.WriteLine(errorMessage);
+				Console.Write("Press the Enter key to quit.");
+				Console.ReadLine();
 			}
 		}
 	}
---
+++ Source/ScreenshotCapturer/ScreenshotCapturer/ScreenshotCapturer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ScreenshotCapturer
+{
+	public class ScreenshotCapturer
+	{
+		public string ImageFilePrefix;
+		public string ImageFileSuffix;
+		public Rectangle? CaptureBoundsNullable;
+		public int ScreenshotsPerSecond;
+		public int ScreenshotsToTakeMax;
+
+		public void Start()
+		{
+			Console.WriteLine("--imageFilePrefix=" + ImageFilePrefix);
+			Console.WriteLine("--imageFileSuffix=" + ImageFileSuffix);
+			Console.WriteLine("--bounds=" + CaptureBoundsNullable);
+			Console.WriteLine("--screenshotsPerSecond=" + ScreenshotsPerSecond);
+			Console.WriteLine("--screenshotsMax=" + ScreenshotsToTakeMax);
+
+			if (ScreenshotsToTakeMax == 1)
+			{
+				CopyScreenToImageFile(0);
+			}
+			else if (ScreenshotsPerSecond == 0)
+			{
+				Console.WriteLine("Press the Enter key to take screenshots, or 'quit' to quit.");
+
+				var userWishesToQuit = false;
+				var screenshotsTakenSoFarCount = 0;
+				while (userWishesToQuit == false)
+				{
+					var userInput = Console.ReadLine();
+					if (userInput == "exit" || userInput == "quit")
+					{
+						userWishesToQuit = true;
+					}
+					else
+					{
+						CopyScreenToImageFile(screenshotsTakenSoFarCount);
+						screenshotsTakenSoFarCount++;
+						if (screenshotsTakenSoFarCount >= ScreenshotsToTakeMax)
+						{
+							userWishesToQuit = true;
+						}
+					}
+				}
+			}
+			else
+			{
+				var millisecondsPerScreenshot =
+					(int)Math.Round(1000.0 / ScreenshotsPerSecond);
+				for (var i = 0; i < ScreenshotsToTakeMax; i++)
+				{
+					CopyScreenToImageFile(i);
+					Thread.Sleep(millisecondsPerScreenshot);
+				}
+			}
+		}
+		public void CopyScreenToImageFile(int? imageIndex)
+		{
+			// Adapted from code found at the URL
+			// https://stackoverflow.com/questions/5049122/capture-the-screen-shot-using-net
+			Rectangle captureBounds;
+			if (CaptureBoundsNullable == null)
+			{
+				captureBounds = Screen.PrimaryScreen.Bounds;
+			}
+			else
+			{
+				captureBounds = CaptureBoundsNullable.Value;
+			}
+			var bitmapCaptured = new Bitmap(captureBounds.Width, captureBounds.Height);
+			var graphics = Graphics.FromImage(bitmapCaptured);
+			graphics.CopyFromScreen
+			(
+				captureBounds.X, captureBounds.Y, // source
+				0, 0, // destination
+				bitmapCaptured.Size,
+				CopyPixelOperation.SourceCopy
+			);
+			var filePathToSaveTo =
+				ImageFilePrefix
+				+ (imageIndex == null ? "" : imageIndex.Value.ToString())
+				+ ImageFileSuffix;
+
+			Console.WriteLine("Saving " + filePathToSaveTo + "...");
+
+			try
+			{
+				bitmapCaptured.Save(filePathToSaveTo);
+			}
+			catch (Exception)
+			{
+				var errorMessage =
+					"Error attemping to save file. Ensure directory exists, and permissions are adequate.";
+				Console.WriteLine(errorMessage);
+			}
+		}
+	}
+}
